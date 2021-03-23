07be3369438827b1b7f3921ab4761cf880a85a95
humayun-ahmad <humayunraj789@gmail.com>
2021-03-23 21:03:07 +0600

    small project calculator completed

--- calculator.php
+++ calculator.php
@@ -11,7 +11,7 @@
 	</head>
 	<body>
 		
-		<div class="container" style="margin-top: 50px">
+		<div class="container" style="margin-top: 50px;">
 
 		    <!-- Calculator form by webdevtrick.com -->
 		    <form method="post" action="calculator.php">
@@ -25,8 +25,45 @@
 		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
 		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
 		    </form>
-	    
+	    				
+	    	<div style="background-color: #2c3e50; height:50px ; width: 40%; margin-top: 20px; color: #fff; text-align: center; padding: 10px; font-weight: 700; font-size: 20px;">
+				<p>
+					<?php 
+						if(isset($_POST['submit'])){
+
+							$number1 = $_POST['number1'];
+							$number2 = $_POST['number2'];
+							$operation = $_POST['operation'];
+
+							switch ($operation) {
+								case 'plus':
+									$add = $number1 + $number2;
+									echo "The Additon will be {$add}";
+									break;
+								case 'minus':
+									$sub = $number1 - $number2;
+									echo "The subtraction will be {$sub}";
+									break;
+								case 'multiply':
+								$mul = $number1 * $number2;
+									echo "The Multiplication will be {$mul}";
+									break;
+								case 'divided by':
+								$div = $number1 / $number2;
+									echo "The division will be {$div}";
+									break;
+								
+								default:
+									echo "something went wrong";
+									break;
+							}
+						}
+					 ?>
+				</p>
+			</div>
 		</div>
+
+		
 	
 	</body>
 </html>
